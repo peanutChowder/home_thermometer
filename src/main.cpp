8423a849edecc20c3c4c3cc0f9072957dbcb714b
Jacob Feng <peanutchowder@gmail.com>
2024-03-04 23:47:27 -0700

    Added placeholder code for connecting to SHT31

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,43 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+
 #include <LiquidCrystal_I2C.h>
+#include "Adafruit_SHT31.h"
 
 #define LCD_ADDRESS 0x27
 
-LiquidCrystal_I2C lcd(LCD_ADDRESS,16,2);
+LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2);
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
+void setup()
+{
+  Serial.begin(9600);
 
-void setup() {
+  // Delay Serial
+  while (!Serial)
+    delay(10);
+
+  if (!sht31.begin(0x44))
+  { // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1)
+      delay(1);
+  }
+
+  Serial.print("Heater Enabled State: ");
+  if (sht31.isHeaterEnabled())
+    Serial.println("ENABLED");
+  else
+    Serial.println("DISABLED");
+
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
   lcd.print("Helloooo");
-
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
